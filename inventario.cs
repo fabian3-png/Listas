d5e77e66b04d461037e091862dd5dfa9c14b785b
fabian3-png <fabianramirezrosaz@gmail.com>
2026-03-26 08:37:58 -0600

    Create Inventario class with product management methods

---
+++ inventario.cs
@@ -0,0 +1,45 @@
+using System;
+
+public class Inventario
+{
+    public int Contadorld {get; set;}
+    
+    //List<Producto> productos=new List<Producto>();
+       
+    
+    public void AgregarProducto()
+    {
+        
+    }
+    public void ListarProductos()
+    {
+        
+    }
+    public void BuscarProducto()
+    {
+        
+    }
+    public void ActualizarStock()
+    {
+        
+    }
+    public void EliminarProducto()
+    {
+        
+    }
+}
+
+public class Producto
+{
+    public int Id {get; set;}
+    public string Nombre {get; set;}
+    public int Stock {get; set;}
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+    }
+}
